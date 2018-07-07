fe19d7ad35141a7f846e3f72f339252ca49c5dfc
Infinibattle <Infinibattle@michael.infitest.nl>
2018-07-07 14:21:25 +0200

    Added unit test project.

---
+++ Collabitama.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Collabitama.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
